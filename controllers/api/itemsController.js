f1f5a6551918b53a8de624485dec5a4904bfa9b8
Mark Agaton <markallenagaton03@gmail.com>
2020-04-16 23:17:52 +0800

    - added initial RESTful API routes - added ejs module

---
+++ controllers/api/itemsController.js
@@ -0,0 +1,34 @@
+exports.index = (req, res) => {
+    response = {};
+    response['route'] = 'index';
+    res.json(response);
+};
+
+exports.show = (req, res) => {
+    response = {};
+    response['route'] = 'show';
+    response['article_id'] = req.params.id;
+
+    res.json(response);
+};
+
+exports.new = (req, res) => {
+    response = {};
+    response['route'] = 'new article';
+    response['method'] = 'POST';
+    res.json(repsonse);
+};
+
+exports.update = (req, res) => {
+    response = {};
+    response['route'] = 'update';
+    response['method'] = 'PUT';
+    res.json(response);
+};
+
+exports.destroy = (req, res) => {
+    response = {};
+    response['route'] = 'destroy';
+    response['method'] = 'DELETE';
+    res.json(response);
+};
---
+++ index.js
@@ -0,0 +1,101 @@
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const api_router = require('./routes/api');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/api', api_router)
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    next(createError(404))
+})
+
+app.use((err, req, res, next) => {
+    res.locals.message = err.message
+    res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+    // render the error page
+    res.status(err.status || 500)
+    res.render("error")
+})
+
+let port = normalizePort(process.env.PORT || '3000')
+app.set('port', port)
+
+let server = http.createServer(app)
+server.listen(port)
+server.on('listening', onListening)
+server.on('error', onError)
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+    let port = parseInt(val, 10)
+
+    if (isNaN(port)) {
+        // named pipe
+        return val
+    }
+
+    if (port >= 0) {
+        // port number
+        return port
+    }
+
+    return false
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+ function onListening() {
+    let addr = server.address()
+    let bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port
+    console.log('Listening on ' + bind)
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    var bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        case 'ECONNRESET':
+            console.error(error)
+            break
+        default:
+            throw error;
+    }
+}
+
+module.exports = app
