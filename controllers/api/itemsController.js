aa9a7c55d9b0dce6811dea2e5f4b52ffbe66355c
Mark Agaton <markallenagaton03@gmail.com>
2020-04-17 01:29:04 +0800

    fully functioning CRUD REST APIs tested using Postman Todo: add GUI

--- controllers/api/itemsController.js
+++ controllers/api/itemsController.js
@@ -1,29 +1,57 @@
 const mysql = require('./mysql_helper');
 
 exports.index_api = (req, res) => {
-    response = {};
-    response['route'] = 'index';
-    res.json(response);
+    let response = {};
+
+    let sql = `SELECT * FROM items;`;
+    mysql.connection.query(sql, (err, results, fields) => {
+        if (err == null) {
+            response['status'] = 'success';
+        } else {
+            response['status'] = 'unsuccessful';
+        }
+
+        response['err'] = err;
+        response['results'] = results;
+        res.json(response);
+    });
 };
 
 exports.show_api = (req, res) => {
-    response = {};
-    response['route'] = 'show';
-    response['article_id'] = req.params.id;
+    let item_id = req.params.id;
 
-    res.json(response);
+    let response = {}
+
+    let sql = `SELECT * FROM items WHERE id = ${item_id}`;
+    mysql.connection.query(sql, (err, results, fields) => {
+        if (err == null) {
+            response['status'] = 'success';
+        } else {
+            response['status'] = 'unsuccessful';
+        }
+
+        response['err'] = err;
+        response['results'] = results;
+        res.json(response);
+    });
 };
 
 exports.new_api = (req, res) => {
-
     let name = req.body.name;
     let qty = req.body.qty;
     let amount = req.body.amount;
 
-    response = {}
+    let response = {}
 
     let sql = `INSERT INTO items (name, qty, amount) VALUES ('${name}', ${qty}, ${amount})`;
     mysql.connection.query(sql, (err, results, fields) => {
+        if (err == null) {
+            response['status'] = 'success';
+        } else {
+            response['status'] = 'unsuccessful';
+        }
+
+        response['err'] = err;
         response['results'] = results;
         response['fields'] = fields;
         
@@ -31,19 +59,48 @@ exports.new_api = (req, res) => {
         console.log(fields);
         res.json(response);
     });
-
 };
 
 exports.update_api = (req, res) => {
-    response = {};
-    response['route'] = 'update';
-    response['method'] = 'PUT';
-    res.json(response);
+    let item_id = req.params.id;
+    let name = req.body.name;
+    let qty = req.body.qty;
+    let amount = req.body.amount;
+    let response = {};
+
+    let sql = `UPDATE items SET name = '${name}', qty = ${qty}, amount = ${amount} WHERE id = ${item_id};`;
+    mysql.connection.query(sql, (err, results, fields) => {
+        if (err == null) {
+            response['status'] = 'success';
+        } else {
+            response['status'] = 'unsuccessful';
+        }
+
+        response['err'] = err;
+        response['results'] = results;
+        response['fields'] = fields;
+        
+        res.json(response);
+    });
 };
 
 exports.destroy_api = (req, res) => {
-    response = {};
-    response['route'] = 'destroy';
-    response['method'] = 'DELETE';
-    res.json(response);
+    let item_id = req.params.id;
+
+    let response = {};
+
+    let sql = `DELETE FROM items WHERE id = ${item_id}`;
+    mysql.connection.query(sql, (err, results, fields) => {
+        if (err == null) {
+            response['status'] = 'success';
+        } else {
+            response['status'] = 'unsuccessful';
+        }
+
+        response['err'] = err;
+        response['results'] = results;
+        response['fields'] = fields;
+        
+        res.json(response);
+    });
 };
