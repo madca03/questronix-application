dfec9a4da96076169653c1035579cee19f749413
Mark Agaton <markallenagaton03@gmail.com>
2020-04-20 02:08:36 +0000

    handle mysql reconnects during PROTOCOL_CONNECTION_LOST error

--- controllers/api/mysql_helper.js
+++ controllers/api/mysql_helper.js
@@ -1,8 +1,33 @@
 const mysql = require('mysql2');
 
-exports.connection = mysql.createConnection({
+const db_config = {
     host: 'localhost',
     user: 'mark',
-    database: 'inventory',
-    password: 'questronix'
-});
+    password: 'questronix',
+    database: 'inventory'
+};
+
+let connection;
+
+function handleDisconnect() {
+    connection = mysql.createConnection(db_config);
+
+    connection.connect((err) => {
+        if (err) {
+            console.log('error when connecting to db: ', err);
+            setTimeout(handleDisconnect, 2000);
+        } 
+    });
+
+    connection.on('error', (err) => {
+        console.log('db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleDisconnect();
+        } else {
+            throw err;
+        }
+    });
+}
+
+handleDisconnect();
+exports.connection = connection;
